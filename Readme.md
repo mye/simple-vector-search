0a9947d0b156e14bd14f0470c27b0f13047d5e03
Lorenz Köhl <lo@lrz.me>
2022-09-27 13:07:46 +0200

    add notebook

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Reverse Dictionary
+
+Writing well is laborsome. A good dictionary helps but it's only usable in one direction.
+You have to think of a word, look it up, chase references and so on. Even more work!
+
+It may be helpful to look up words by meaning, by what we as writers want to express.
+For example, if we had a function `find_words("I'm lost for words")`
+and it would present us with a choice of words:
+
+```
+astoundment, bewildered, blank, confus, distraught, perplexly, stagger, stound, unyielded
+```
+
+then we may find the right word we want, without all the gyrations of traditional dictionary use.
+
+Can we implement this function? The answer is yes and it's not hard (*if you know your way around python*)!
+
+see `reverse_dictionary.ipynb`
--- cleanwebst.py
+++ cleanwebst.py
@@ -5,7 +5,7 @@ from html5_parser import parse
 classdivs = lambda p, c: [''.join(e.itertext()) for e in p.xpath(f'//div[@class="{c}"]')]
 
 # get all text in a node as a flat list
-nodetexts = lambda n, cs: [text for elem in (classdivs(n, c) for c in cs) for text in elem]
+nodetexts = lambda n, cs: [text.strip() for elem in (classdivs(n, c) for c in cs) for text in elem]
 
 # get a list of definitions text for relevant dictionary entries
 divdefs = lambda n: nodetexts(n, ('def', 'q', 'ety', 'cs', 'note'))
@@ -14,7 +14,7 @@ divdefs = lambda n: nodetexts(n, ('def', 'q', 'ety', 'cs', 'note'))
 if __name__ == '__main__':
 	webst = json.load(sys.stdin)
 
-	webst_clean = {entry: defs
+	webst_clean = {entry.lower(): defs
 		for entry in webst
 		if (defs := divdefs(parse(webst[entry]))) # skip empty defs
 	}
